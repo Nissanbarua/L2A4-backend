40028c6d82419263660bbacc4444a4f6d8f4c40f
Nissan Barua <baruanissan55@gmail.com>
2025-07-31 00:49:49 +0600

    in controllers CRUD operation added

--- src/controllers/book.controller.ts
+++ src/controllers/book.controller.ts
@@ -15,3 +15,24 @@ export const getBookById = async (req: Request, res: Response) => {
 };
 
 // create new book
+export const createBook = async (req: Request, res: Response) => {
+  const book = new Book(req.body);
+  await book.save();
+  res.status(201).json(book);
+};
+
+// UpdateBook
+export const updateBook = async (req: Request, res: Response) => {
+  const updateBook = await Book.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  if (!updateBook) return res.status(404).json({ message: "Book Not Found" });
+  res.json(updateBook);
+};
+
+// deleteBook
+export const deleteBook = async (req: Request, res: Response) => {
+  const deleteBook = await Book.findByIdAndDelete(req.params.id);
+  if (!deleteBook) return res.status(404).json({ message: "BOOK NOT FOUND" });
+  res.json({ message: "Book Deleted Succesfully" });
+};

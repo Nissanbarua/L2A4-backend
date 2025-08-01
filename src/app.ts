fab87cc7e1652bb6629565c3abc6a3f481adf9c1
Nissan Barua <baruanissan55@gmail.com>
2025-08-01 11:54:21 +0600

    some major bug fixed

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,14 @@ import { bookRoutes } from "./routes/book.route";
 import { borrowRoutes } from "./routes/borrow.route";
 
 const app = express();
-
 app.use(express.json());
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'https://library-frontend-pearl.vercel.app']
-   })
+    origin: [
+      "https://library-frontend-sandy-beta.vercel.app",
+      "http://localhost:5173",
+    ],
+  })
 );
 
 app.use("/api/books", bookRoutes);
@@ -17,5 +19,15 @@ app.use("/api", borrowRoutes);
 app.get("/", (req, res) => {
   res.send("Library API is running");
 });
+
+// // Routes
+// app.get("/api/books", async (req, res) => {
+//   try {
+//     // Example dummy data — replace with real DB query
+//     res.json([{ title: "Atomic Habits", author: "James Clear" }]);
+//   } catch (err) {
+//     res.status(500).json({ error: "Server Error" });
+//   }
+// });
 
 export default app;
